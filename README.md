54ab3a19b8dd2a4b01e10eaebdf3cf4b3e9727d6
Eugene Maksymenko <maxigene@gmail.com>
2022-12-05 01:38:09 +0200

    Release 1.1.3.

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'earth.worldwind:worldwind:1.1.2'
+    implementation 'earth.worldwind:worldwind:1.1.3'
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 
 allprojects {
     group = "earth.worldwind"
-    version = "1.1.2"
+    version = "1.1.3"
 
     extra.apply {
         set("minSdk", 26) // java.time requires Oreo. Use "isCoreLibraryDesugaringEnabled = true" to lower API to KitKat
