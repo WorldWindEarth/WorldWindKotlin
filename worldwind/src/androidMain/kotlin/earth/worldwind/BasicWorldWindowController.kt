7f681c937931e3d03b0adc47ef8194f61c013a58
Eugene Maksymenko <maxigene@gmail.com>
2022-12-25 21:30:19 +0200

    Render pick frame on gesture begin to ensure terrain availability outside last rendered terrain.

--- worldwind/src/androidMain/kotlin/earth/worldwind/BasicWorldWindowController.kt
+++ worldwind/src/androidMain/kotlin/earth/worldwind/BasicWorldWindowController.kt
@@ -235,7 +235,11 @@ open class BasicWorldWindowController(wwd: WorldWindow): WorldWindowController, 
 
     protected open fun gestureDidBegin() {
         if (activeGestures++ == 0) {
-            wwd.engine.cameraAsLookAt(beginLookAt)
+            // Render pick frame to ensure terrain availability outside last rendered terrain.
+            val terrainPosition = wwd.pick(
+                wwd.engine.viewport.width / 2f, wwd.engine.viewport.height / 2f
+            ).terrainPickedObject?.terrainPosition
+            wwd.engine.cameraAsLookAt(beginLookAt, terrainPosition)
             lookAt.copy(beginLookAt)
         }
     }
