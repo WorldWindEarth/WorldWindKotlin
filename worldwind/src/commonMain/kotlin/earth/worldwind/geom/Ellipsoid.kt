475d09365f95e263c53e1074f07e73ca49033a30
Eugene Maksymenko <maxigene@gmail.com>
2023-01-20 01:01:36 +0200

    Make Ellipsoid and open class to be able to implement additional ellipsoids.

--- worldwind/src/commonMain/kotlin/earth/worldwind/geom/Ellipsoid.kt
+++ worldwind/src/commonMain/kotlin/earth/worldwind/geom/Ellipsoid.kt
@@ -1,9 +1,11 @@
 package earth.worldwind.geom
 
+import kotlin.jvm.JvmStatic
+
 /**
  * Oblate ellipsoid with semi-major axis and inverse flattening.
  */
-enum class Ellipsoid(
+open class Ellipsoid(
     /**
      * One half of the ellipsoid's major axis length in meters, which runs through the center to opposite points on the
      * equator.
@@ -15,12 +17,6 @@ enum class Ellipsoid(
      */
     val inverseFlattening: Double
 ) {
-    /**
-     * WGS 84 reference ellipsoid for Earth. The ellipsoid's semi-major axis and inverse flattening factor are
-     * configured according to the WGS 84 reference system (aka WGS 1984, EPSG:4326). WGS 84 reference values taken from
-     * [here](http://earth-info.nga.mil/GandG/publications/NGA_STND_0036_1_0_0_WGS84/NGA.STND.0036_1.0.0_WGS84.pdf).
-     */
-    WGS84(6378137.0, 298.257223563);
     /**
      * Computes this ellipsoid's semi-minor length axis in meters. The semi-minor axis is one half of the ellipsoid's
      * minor axis, which runs through the center to opposite points on the poles.
@@ -38,5 +34,31 @@ enum class Ellipsoid(
         return 2 * f - f * f
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Ellipsoid) return false
+
+        if (semiMajorAxis != other.semiMajorAxis) return false
+        if (inverseFlattening != other.inverseFlattening) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = semiMajorAxis.hashCode()
+        result = 31 * result + inverseFlattening.hashCode()
+        return result
+    }
+
     override fun toString() = "Ellipsoid(semiMajorAxis=$semiMajorAxis, inverseFlattening=$inverseFlattening)"
+
+    companion object {
+        /**
+         * WGS 84 reference ellipsoid for Earth. The ellipsoid's semi-major axis and inverse flattening factor are
+         * configured according to the WGS 84 reference system (aka WGS 1984, EPSG:4326). WGS 84 reference values taken from
+         * [here](http://earth-info.nga.mil/GandG/publications/NGA_STND_0036_1_0_0_WGS84/NGA.STND.0036_1.0.0_WGS84.pdf).
+         */
+        @JvmStatic
+        val WGS84 = Ellipsoid(6378137.0, 298.257223563)
+    }
 }
