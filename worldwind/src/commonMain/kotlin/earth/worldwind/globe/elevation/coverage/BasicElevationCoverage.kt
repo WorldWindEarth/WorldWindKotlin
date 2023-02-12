3ce55e87cc6e506df6d315e16041640426c53dfe
Eugene Maksymenko <maxigene@gmail.com>
2023-02-12 23:27:17 +0200

    Fix basic elevation coverage layers order.

--- worldwind/src/commonMain/kotlin/earth/worldwind/globe/elevation/coverage/BasicElevationCoverage.kt
+++ worldwind/src/commonMain/kotlin/earth/worldwind/globe/elevation/coverage/BasicElevationCoverage.kt
@@ -5,10 +5,11 @@ import earth.worldwind.geom.Sector
 import earth.worldwind.ogc.WmsElevationCoverage
 
 /**
- * Displays NASA's global elevation coverage at 90m (3 arc-second) resolution and 900m resolution on the ocean floor,
- * all from an OGC Web Map Service (WMS). By default, BasicElevationCoverage is configured to
- * retrieve elevation coverage from the WMS at [&amp;https://wms.worldwind.earth/elev](https://wms.worldwind.earth/elev?SERVICE=WMS&amp;REQUEST=GetCapabilities).
+ * Displays NASA's global elevation coverage at 90m (3 arc-second) resolution and 900m resolution on the ocean floor.
+ * By default, BasicElevationCoverage is configured to retrieve elevation coverage from the WMS at
+ * [&amp;https://wms.worldwind.earth/elev](https://wms.worldwind.earth/elev?SERVICE=WMS&amp;REQUEST=GetCapabilities).
  */
 class BasicElevationCoverage : WmsElevationCoverage(
-    "https://wms.worldwind.earth/elev", "SRTM-CGIAR,GEBCO", "application/bil16", Sector().setFullSphere(), Angle.fromSeconds(3.0)
+    "https://wms.worldwind.earth/elev", "GEBCO,SRTM-CGIAR", "application/bil16",
+    Sector().setFullSphere(), Angle.fromSeconds(3.0)
 )
