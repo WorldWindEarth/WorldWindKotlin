82b2d4c1b4f9b7ed23c57b2ee46fd94161502bfc
Eugene Maksymenko <maxigene@gmail.com>
2023-03-29 22:21:33 +0300

    Release 1.2.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 allprojects {
     group = "earth.worldwind"
-    version = "1.2.4"
+    version = "1.2.5"
 
     extra.apply {
         set("minSdk", 26) // java.time requires Oreo. Use "isCoreLibraryDesugaringEnabled = true" to lower API to KitKat
