4e8d3e6fa5f646a0f71f7156952ae162b30d27ab
Eugene Maksymenko <maxigene@gmail.com>
2023-09-28 22:38:32 +0300

    Release 1.3.0.

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'earth.worldwind:worldwind:1.2.29'
+    implementation 'earth.worldwind:worldwind:1.3.0'
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 allprojects {
     group = "earth.worldwind"
-    version = "1.2.29"
+    version = "1.3.0"
 
     extra.apply {
         set("minSdk", 21)
