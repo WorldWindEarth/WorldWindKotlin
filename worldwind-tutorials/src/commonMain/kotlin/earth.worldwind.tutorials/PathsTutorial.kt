3ec9428619f34dfc47c625a2edfa5adba15117cb
Anon <>
2024-01-21 12:59:19 +0200

    First commit, create required files, play around with vertex generation

--- worldwind-tutorials/src/commonMain/kotlin/earth.worldwind.tutorials/PathsTutorial.kt
+++ worldwind-tutorials/src/commonMain/kotlin/earth.worldwind.tutorials/PathsTutorial.kt
@@ -7,69 +7,89 @@ import earth.worldwind.geom.Angle.Companion.degrees
 import earth.worldwind.geom.Position
 import earth.worldwind.layer.RenderableLayer
 import earth.worldwind.render.Color
+import earth.worldwind.shape.GeomPath
 import earth.worldwind.shape.Path
+import earth.worldwind.shape.PathType
 
 class PathsTutorial(private val engine: WorldWind) : AbstractTutorial() {
 
     private val layer = RenderableLayer("Paths").apply {
-        // Create a basic path with the default attributes, the default altitude mode (ABSOLUTE),
-        // and the default path type (GREAT_CIRCLE).
         addRenderable(
-            Path(
+            GeomPath(
                 listOf(
                     Position.fromDegrees(50.0, -180.0, 1e5),
                     Position.fromDegrees(30.0, -100.0, 1e6),
                     Position.fromDegrees(50.0, -40.0, 1e5)
                 )
+            ).apply{pathType = PathType.LINEAR}
+        )
+        addRenderable(
+            Path(
+                listOf(
+                    Position.fromDegrees(40.0, -180.0, 1e5),
+                    Position.fromDegrees(20.0, -100.0, 1e6),
+                    Position.fromDegrees(40.0, -40.0, 1e5)
+                )
             )
         )
-
-        // Create a terrain following path with the default attributes, and the default path type (GREAT_CIRCLE).
-        addRenderable(
-            Path(
-                listOf(
-                    Position.fromDegrees(40.0, -180.0, 0.0),
-                    Position.fromDegrees(20.0, -100.0, 0.0),
-                    Position.fromDegrees(40.0, -40.0, 0.0)
-                )
-            ).apply {
-                altitudeMode = AltitudeMode.CLAMP_TO_GROUND // clamp the path vertices to the ground
-                isFollowTerrain = true // follow the ground between path vertices
-            }
-        )
-
-        // Create an extruded path with the default attributes, the default altitude mode (ABSOLUTE),
-        // and the default path type (GREAT_CIRCLE).
-        addRenderable(
-            Path(
-                listOf(
-                    Position.fromDegrees(30.0, -180.0, 1e5),
-                    Position.fromDegrees(10.0, -100.0, 1e6),
-                    Position.fromDegrees(30.0, -40.0, 1e5)
-                )
-            ).apply {
-                isExtrude = true // extrude the path from the ground to each path position's altitude
-            }
-        )
-
-        // Create an extruded path with custom attributes that display the extruded vertical lines,
-        // make the extruded interior 50% transparent, and increase the path line with.
-        addRenderable(
-            Path(
-                listOf(
-                    Position.fromDegrees(20.0, -180.0, 1e5),
-                    Position.fromDegrees(0.0, -100.0, 1e6),
-                    Position.fromDegrees(20.0, -40.0, 1e5)
-                )
-            ).apply {
-                attributes.apply {
-                    isDrawVerticals = true // display the extruded verticals
-                    interiorColor = Color(1f, 1f, 1f, 0.5f) // 50% transparent white
-                    outlineWidth = 3f
-                }
-                isExtrude = true // extrude the path from the ground to each path position's altitude
-            }
-        )
+//        // Create a basic path with the default attributes, the default altitude mode (ABSOLUTE),
+//        // and the default path type (GREAT_CIRCLE).
+//        addRenderable(
+//            Path(
+//                listOf(
+//                    Position.fromDegrees(50.0, -180.0, 1e5),
+//                    Position.fromDegrees(30.0, -100.0, 1e6),
+//                    Position.fromDegrees(50.0, -40.0, 1e5)
+//                )
+//            )
+//        )
+//
+//        // Create a terrain following path with the default attributes, and the default path type (GREAT_CIRCLE).
+//        addRenderable(
+//            Path(
+//                listOf(
+//                    Position.fromDegrees(40.0, -180.0, 0.0),
+//                    Position.fromDegrees(20.0, -100.0, 0.0),
+//                    Position.fromDegrees(40.0, -40.0, 0.0)
+//                )
+//            ).apply {
+//                altitudeMode = AltitudeMode.CLAMP_TO_GROUND // clamp the path vertices to the ground
+//                isFollowTerrain = true // follow the ground between path vertices
+//            }
+//        )
+//
+//        // Create an extruded path with the default attributes, the default altitude mode (ABSOLUTE),
+//        // and the default path type (GREAT_CIRCLE).
+//        addRenderable(
+//            Path(
+//                listOf(
+//                    Position.fromDegrees(30.0, -180.0, 1e5),
+//                    Position.fromDegrees(10.0, -100.0, 1e6),
+//                    Position.fromDegrees(30.0, -40.0, 1e5)
+//                )
+//            ).apply {
+//                isExtrude = true // extrude the path from the ground to each path position's altitude
+//            }
+//        )
+//
+//        // Create an extruded path with custom attributes that display the extruded vertical lines,
+//        // make the extruded interior 50% transparent, and increase the path line with.
+//        addRenderable(
+//            Path(
+//                listOf(
+//                    Position.fromDegrees(20.0, -180.0, 1e5),
+//                    Position.fromDegrees(0.0, -100.0, 1e6),
+//                    Position.fromDegrees(20.0, -40.0, 1e5)
+//                )
+//            ).apply {
+//                attributes.apply {
+//                    isDrawVerticals = true // display the extruded verticals
+//                    interiorColor = Color(1f, 1f, 1f, 0.5f) // 50% transparent white
+//                    outlineWidth = 3f
+//                }
+//                isExtrude = true // extrude the path from the ground to each path position's altitude
+//            }
+//        )
     }
 
     override fun start() {
