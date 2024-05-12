c8202f7eaa4739a80a284632254cd1cc940d933b
Eugene Maksymenko <maxigene@gmail.com>
2024-05-12 15:34:00 +0300

    Release 1.5.5.

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'earth.worldwind:worldwind:1.5.2'
+    implementation 'earth.worldwind:worldwind:1.5.5'
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 allprojects {
     group = "earth.worldwind"
-    version = "1.5.2"
+    version = "1.5.5"
 
     extra.apply {
         set("minSdk", 21)
