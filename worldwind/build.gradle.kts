95014f572cd8e77150c06caafa1503da489503ea
Eugene Maksymenko <maxigene@gmail.com>
2024-06-07 22:21:08 +0300

    Fix MercatorTiledSurfaceImage colning.

--- worldwind/build.gradle.kts
+++ worldwind/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
         val ormliteVersion = "6.1"
         val commonMain by getting {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+                api("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
@@ -113,8 +113,8 @@ kotlin {
         val androidMain by getting {
             dependsOn(jvmCommonMain)
             dependencies {
-                implementation("androidx.annotation:annotation:1.7.1")
-                implementation("androidx.appcompat:appcompat-resources:1.6.1")
+                implementation("androidx.annotation:annotation:1.8.0")
+                implementation("androidx.appcompat:appcompat-resources:1.7.0")
                 implementation("io.github.missioncommand:mil-sym-android-renderer:0.1.60")
                 implementation("com.j256.ormlite:ormlite-android:$ormliteVersion")
             }
--- worldwind/src/commonMain/kotlin/earth/worldwind/layer/mercator/MercatorTiledSurfaceImage.kt
+++ worldwind/src/commonMain/kotlin/earth/worldwind/layer/mercator/MercatorTiledSurfaceImage.kt
@@ -1,11 +1,15 @@
 package earth.worldwind.layer.mercator
 
+import earth.worldwind.layer.mercator.MercatorSector.Companion.fromSector
 import earth.worldwind.shape.TiledSurfaceImage
 import earth.worldwind.util.LevelSet
 import earth.worldwind.util.TileFactory
 
 open class MercatorTiledSurfaceImage(tileFactory: TileFactory, levelSet: LevelSet) : TiledSurfaceImage(tileFactory, levelSet) {
-    override fun clone() = MercatorTiledSurfaceImage(tileFactory, LevelSet(levelSet)).also {
+    override fun clone() = MercatorTiledSurfaceImage(tileFactory, with(levelSet) {
+        // It is crucial to keep the Mercator sector in the level set clone to keep correct tiling
+        LevelSet(fromSector(sector), fromSector(tileOrigin), firstLevelDelta, numLevels, tileWidth, tileHeight, levelOffset)
+    }).also {
         it.imageOptions = imageOptions
     }
 
