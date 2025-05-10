eebbed70ec5a16c3e804bc32b3e358360014ac7c
Eugene Maksymenko <maxigene@gmail.com>
2025-05-10 03:11:13 +0300

    Update Gradle version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:8.7.3")
+                useModule("com.android.tools.build:gradle:8.9.2")
             }
         }
     }
