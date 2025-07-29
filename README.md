88d7b2f8f9782dc417e95b0b930ff2d77471fab7
Eugene Maksymenko <maxigene@gmail.com>
2025-07-30 02:09:17 +0300

    Update MilSym libraries.

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ repositories {
 }
 
 dependencies {
-    implementation 'earth.worldwind:worldwind:1.8.0'
+    implementation 'earth.worldwind:worldwind:1.8.1'
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 
 allprojects {
     group = "earth.worldwind"
-    version = "1.8.0"
+    version = "1.8.1"
 
     extra.apply {
         set("minSdk", 24)
