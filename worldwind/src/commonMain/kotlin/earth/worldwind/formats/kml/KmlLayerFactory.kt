0f6fc6eae2fa7cfa8cb60856e7b4b8aeb0ee2104
Oleksandr Havryliuk <havryliuk.oleksandr.o@gmail.com>
2025-08-27 14:27:31 +0300

    Add KmlLayer user properties (id, sector, and lookAt)

--- worldwind/src/commonMain/kotlin/earth/worldwind/formats/kml/KmlLayerFactory.kt
+++ worldwind/src/commonMain/kotlin/earth/worldwind/formats/kml/KmlLayerFactory.kt
@@ -1,21 +1,46 @@
 package earth.worldwind.formats.kml
 
+import earth.worldwind.formats.kml.models.LookAt
 import earth.worldwind.formats.kml.models.Style
 import earth.worldwind.formats.kml.models.StyleMap
+import earth.worldwind.geom.AltitudeMode
+import earth.worldwind.geom.Angle
+import earth.worldwind.geom.Angle.Companion.ZERO
+import earth.worldwind.geom.Ellipsoid
+import earth.worldwind.geom.Position
+import earth.worldwind.geom.Sector
 import earth.worldwind.layer.RenderableLayer
 import earth.worldwind.render.Renderable
+import earth.worldwind.shape.Ellipse
 import earth.worldwind.shape.Label
+import earth.worldwind.shape.Path
+import earth.worldwind.shape.Placemark
+import earth.worldwind.shape.Polygon
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import nl.adaptivity.xmlutil.XmlUtilInternal
 import nl.adaptivity.xmlutil.core.impl.multiplatform.Reader
 import nl.adaptivity.xmlutil.core.impl.multiplatform.StringReader
+import kotlin.math.cos
 
 @OptIn(XmlUtilInternal::class)
 object KmlLayerFactory {
     private const val KML_LAYER_NAME = "KML Layer"
     private val kml = KML()
     private val converter = KmlToRenderableConverter()
+
+    const val KML_LAYER_ID_KEY = "KMLLayerId"
+    const val KML_LAYER_SECTOR_KEY = "KMLLayerSector"
+    const val KML_LAYER_LOOK_AT_KEY = "KMLLayerLookAt"
+
+    private val METERS_PER_LATITUDE_DEGREE = 111_320.0
+
+    private data class KmlLayerData(
+        val id: String,
+        val displayName: String?,
+        val lookAt: earth.worldwind.geom.LookAt?,
+        val renderables: List<Renderable>,
+    )
 
     suspend fun createLayer(
         text: String,
@@ -28,13 +53,8 @@ object KmlLayerFactory {
         displayName: String? = KML_LAYER_NAME,
         labelVisibilityThreshold: Double = 0.0
     ) = RenderableLayer(displayName).apply {
-        isPickEnabled = false // Layer is not pickable by default
-        decodeFromReader(reader).map { (_, renderables) ->
-            if (labelVisibilityThreshold != 0.0) renderables.filterIsInstance<Label>()
-                .forEach { label ->
-                    label.visibilityThreshold = labelVisibilityThreshold
-                }
-            addAllRenderables(renderables)
+        decodeFromReader(reader).map { data ->
+            setLayerData(labelVisibilityThreshold, data)
         }
     }
 
@@ -46,20 +66,32 @@ object KmlLayerFactory {
     suspend fun createLayers(
         reader: Reader,
         labelVisibilityThreshold: Double = 0.0
-    ) = decodeFromReader(reader).map { (name, renderables) ->
-        if (labelVisibilityThreshold != 0.0) renderables.filterIsInstance<Label>()
+    ) = decodeFromReader(reader).map { data ->
+        RenderableLayer(data.displayName).apply {
+            setLayerData(labelVisibilityThreshold, data)
+        }
+    }
+
+    private fun RenderableLayer.setLayerData(
+        labelVisibilityThreshold: Double,
+        data: KmlLayerData
+    ) {
+        isPickEnabled = false // Layer is not pickable by default
+        if (labelVisibilityThreshold != 0.0) data.renderables.filterIsInstance<Label>()
             .forEach { label ->
                 label.visibilityThreshold = labelVisibilityThreshold
             }
-        RenderableLayer(name).apply {
-            isPickEnabled = false // Layer is not pickable by default
-            addAllRenderables(renderables)
-        }
+
+        addAllRenderables(data.renderables)
+
+        putUserProperty(KML_LAYER_ID_KEY, data.id)
+        putUserProperty(KML_LAYER_SECTOR_KEY, computeSector(data.renderables))
+        data.lookAt?.let { putUserProperty(KML_LAYER_LOOK_AT_KEY, data.lookAt) }
     }
 
     private suspend fun decodeFromReader(
         reader: Reader
-    ): Map<String, List<Renderable>> = withContext(Dispatchers.Default) {
+    ): List<KmlLayerData> = withContext(Dispatchers.Default) {
         val styles = mutableMapOf<String, Style>()
         val styleMaps: MutableMap<String, StyleMap> = mutableMapOf()
         val eventsAwaitingStyle = mutableMapOf<String, MutableList<KML.KmlEvent.KmlPlacemark>>()
@@ -68,17 +100,20 @@ object KmlLayerFactory {
         val renderables = mutableMapOf<String, MutableList<Renderable>>()
         // parentId to name
         val names = mutableMapOf<String, String>()
+        val lookAts = mutableMapOf<String, earth.worldwind.geom.LookAt?>()
 
         kml.decodeFromReader(reader).collect { event ->
             when (event) {
                 is KML.KmlEvent.KmlDocument -> {
                     names[event.id] = event.name
                     renderables[event.id] = mutableListOf()
+                    lookAts[event.id] = event.lookAt?.toGeomLookAt()
                 }
 
                 is KML.KmlEvent.KmlFolder -> {
                     names[event.id] = event.name
                     renderables[event.id] = mutableListOf()
+                    lookAts[event.id] = event.lookAt?.toGeomLookAt()
                 }
 
                 is KML.KmlEvent.KmlStyleMap -> {
@@ -155,8 +190,97 @@ object KmlLayerFactory {
         }
 
         renderables
-            .mapValues { entry -> entry.value }
             .filter { it.value.isNotEmpty() } // Filter out empty renderable lists
-            .mapKeys { names[it.key] ?: it.key }
+            .map { entry ->
+                KmlLayerData(
+                    id = entry.key,
+                    displayName = names[entry.key],
+                    lookAt = lookAts[entry.key],
+                    renderables = entry.value,
+                )
+            }
+    }
+
+    private fun LookAt.toGeomLookAt(): earth.worldwind.geom.LookAt? {
+        val latitude = latitude ?: return null
+        val longitude = longitude ?: return null
+        val range = range ?: return null
+
+        return earth.worldwind.geom.LookAt(
+            position = Position.fromDegrees(
+                latitudeDegrees = latitude,
+                longitudeDegrees = longitude,
+                altitude = altitude ?: 0.0
+            ),
+            altitudeMode = AltitudeMode.ABSOLUTE,
+            range = range,
+            heading = Angle.fromDegrees(heading ?: 0.0),
+            tilt = Angle.fromDegrees(tilt ?: 0.0),
+            roll = ZERO
+        )
+    }
+
+    /**
+     * Compute a bounding sector for a list of renderables. The sector is expanded by a margin
+     * fraction to ensure the renderables are not too close to the edge of the sector.
+     */
+    private fun computeSector(
+        renderables: List<Renderable>,
+        marginFraction: Double = 0.05
+    ): Sector {
+        var minLat = Double.POSITIVE_INFINITY
+        var maxLat = Double.NEGATIVE_INFINITY
+        var minLon = Double.POSITIVE_INFINITY
+        var maxLon = Double.NEGATIVE_INFINITY
+
+        fun Position.check() {
+            val lat = latitude.inDegrees
+            val lon = longitude.inDegrees
+
+            if (lat < minLat) minLat = lat
+            if (lat > maxLat) maxLat = lat
+            if (lon < minLon) minLon = lon
+            if (lon > maxLon) maxLon = lon
+        }
+
+        renderables.forEach { renderable ->
+            when (renderable) {
+                is Label -> renderable.position.check()
+                is Placemark -> renderable.position.check()
+                is Path -> renderable.positions.forEach { it.check() }
+                is Polygon -> {
+                    for (index in 0 until renderable.boundaryCount) {
+                        renderable.getBoundary(index).forEach { it.check() }
+                    }
+                }
+
+                is Ellipse -> {
+                    val majorRadius = renderable.majorRadius
+                    val minorRadius = renderable.minorRadius
+                    val center = renderable.center
+                    val lat = center.latitude.inDegrees
+                    val lon = center.longitude.inDegrees
+                    val latDelta = (majorRadius / METERS_PER_LATITUDE_DEGREE)
+                    val lonDelta =
+                        (minorRadius / (METERS_PER_LATITUDE_DEGREE * cos(center.latitude.inRadians)))
+                    if (lat - latDelta < minLat) minLat = lat - latDelta
+                    if (lat + latDelta > maxLat) maxLat = lat + latDelta
+                    if (lon - lonDelta < minLon) minLon = lon - lonDelta
+                    if (lon + lonDelta > maxLon) maxLon = lon + lonDelta
+                }
+            }
+        }
+
+        val deltaLat = maxLat - minLat
+        val deltaLon = maxLon - minLon
+        val latMargin = deltaLat * marginFraction
+        val lonMargin = deltaLon * marginFraction
+
+        return Sector.fromDegrees(
+            minLatDegrees = minLat - latMargin,
+            minLonDegrees = minLon - lonMargin,
+            deltaLatDegrees = deltaLat + 2 * latMargin,
+            deltaLonDegrees = deltaLon + 2 * lonMargin
+        )
     }
 }
